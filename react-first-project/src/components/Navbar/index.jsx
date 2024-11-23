bddc4b365a369e01b52ac3a8156f44d5883d3e12
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-11-23 00:06:20 -0300

    react structure

---
+++ react-first-project/src/components/Navbar/index.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+        <div>
+            <p>Eventos mal</p>
+            <input placeholder="Elige tu proximo evento" />
+        </div>
+    );
+};
+
+export default Navbar;
